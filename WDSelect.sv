f9ee6fa53f5f4205cb6d45b48c7192f257bd27d6
Nila Chenari <nilachenari@gmail.com>
2024-06-27 10:31:59 +0330

    initial commit

--- WDSelect.sv
+++ WDSelect.sv
@@ -2,4 +2,4 @@ module WDSel(input  logic [31:0] WDCop, WriteData,
            input  logic sel, // 1 if cop
            output logic [31:0] WDFinal);
   assign WDFinal = (sel == 1) ? WDCop : WriteData;
-endmodule
+endmodule 
